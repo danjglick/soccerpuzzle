5f02afd300187c5a8402502fc83201e4cfc3cc67
Dan Glick <danglick@Dans-MacBook-Air.local>
2026-02-03 17:31:37 -0500

    change wormhole color

--- main.js
+++ main.js
@@ -759,7 +759,7 @@ function drawWall() {
 }
 
 function drawWormhole() {
-  let color = "purple"
+  let color = "#572991"
   for (let wormhole of [wormholeA, wormholeB]) {
     ctx.beginPath()
     ctx.arc(wormhole.xPos, wormhole.yPos, BALL_RADIUS * .75, 0, 2 * Math.PI)
